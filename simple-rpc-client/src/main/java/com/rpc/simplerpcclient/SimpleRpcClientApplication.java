91f17654409fa5907d8b0b8e130eba01e4a97ddf
mjlwolf <1264744394@qq.com>
2023-11-23 21:29:03 +0800

    [main]: Add to implement a simple RPC framework

--- simple-rpc-client/src/main/java/com/rpc/simplerpcclient/SimpleRpcClientApplication.java
+++ simple-rpc-client/src/main/java/com/rpc/simplerpcclient/SimpleRpcClientApplication.java
@@ -1,4 +1,4 @@
-package com.apollo.rpc;
+package com.rpc.simplerpcclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- simple-rpc-client/src/test/java/com/rpc/simplerpcclient/SimpleRpcClientApplicationTests.java
+++ simple-rpc-client/src/test/java/com/rpc/simplerpcclient/SimpleRpcClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.apollo.rpc;
+package com.rpc.simplerpcclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
